82fe7f34af7c648820d9676896c36ed7c7b213bd
Achrafcodes <essoussygamer@gmail.com>
2023-07-17 17:50:49 +0100

    done

--- src/Components/landing/LandingComponent/Landing.jsx
+++ src/Components/landing/LandingComponent/Landing.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import "../main.css";
-import HeroImg from "../../../assets/undraw_crypto_portfolio_2jy5.svg";
+import { BsTwitter, BsDiscord } from "react-icons/bs";
 import { useSelector } from "react-redux";
 function Landing() {
   let darkmode = useSelector((state) => state.darkmode.darkmode);
   return (
-    <article className={darkmode ? "dark-mode" : "ligth-mode"}>
-      <img src={HeroImg} className="hero-img" alt="" />
+    <article>
       <section>
         <h1>
           Welcome to{" "}
-          <span>
-            Coin<span>Fizz</span>
+          <span className="text-white">
+            Coin<span className="fizz">Fizz</span>
           </span>
-          : The Future of Digital Currency!
+          : The Future of Digital Currency!{" "}
         </h1>
+        <p>
+          Discover CoinFizz: Your gateway to the future of digital currency.
+          Buy, sell, and trade cryptocurrencies effortlessly on our secure and
+          user-friendly platform. Join us now and embark on a thrilling journey
+          into the world of decentralized finance.
+        </p>
       </section>
+      <div className="buttons">
+        <button className={darkmode ? "dark-button" : "ligth-button"}>
+          Twitter <BsTwitter />{" "}
+        </button>
+        <button className={darkmode ? "ligth-button" : "dark-button"}>
+          Discord <BsDiscord />{" "}
+        </button>
+      </div>
     </article>
   );
 }
