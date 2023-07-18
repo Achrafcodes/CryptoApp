fc02470bf15786ca88662cdfd084be1d902a9e87
Achrafcodes <essoussygamer@gmail.com>
2023-07-18 14:24:27 +0100

    done from hero

--- src/Components/landing/CryptoPrices/CryptoContainer.jsx
+++ src/Components/landing/CryptoPrices/CryptoContainer.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-
 function CryptoContainer() {
-  let [data, setdata] = useState(null);
+  let [data, setdata] = useState([]);
   useEffect(() => {
     fetch(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false"
@@ -9,11 +8,18 @@ function CryptoContainer() {
       .then((response) => response.json())
       .then((jsonData) => setdata(jsonData))
       .catch((error) => {
-        console.error("faild", error);
+        console.error("failed", error);
       });
   }, []);
   console.log(data);
-  return <div>CryptoContainer</div>;
+  return (
+    <div className="bg-black text-white overflow-y-scroll ">
+      {/* {data.map((d, i) => (
+
+        <img src={d.image} key={i} alt="" />
+      ))} */}
+    </div>
+  );
 }
 
 export default CryptoContainer;
