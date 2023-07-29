15f783d1258d862da209bc8d6bb8f53bb733cf5f
Achrafcodes <essoussygamer@gmail.com>
2023-07-29 16:20:00 +0100

    market done

--- src/Components/landing/ChoseUs/ChoseUs.jsx
+++ src/Components/landing/ChoseUs/ChoseUs.jsx
@@ -1,5 +1,31 @@
+import data from "./data";
+import Slideshow from "../../ReusableCompo/Slideshow";
 function ChoseUs() {
-  return <div>ChoseUs</div>;
+  //Todo make a caroussel for every chose us part
+
+  return (
+    <div className="ChoseUs h-screen overflow-hidden">
+      <div className="flex px-12 flex-row h-auto ">
+        <Slideshow interval={3600}>
+          {" "}
+          {data.map((data, i) => (
+            <div
+              key={i}
+              className={`car-slide ${
+                i % 2 === 0 ? "flex-row" : "flex-row-reverse"
+              }`}
+            >
+              <div className=" text-start">
+                <h2 className="text-4xl font-bold">{data.title}</h2>
+                <p className="text-xl w-[80%]">{data.description}</p>
+              </div>{" "}
+              <img className="" src={data.image} alt="" />
+            </div>
+          ))}
+        </Slideshow>
+      </div>
+    </div>
+  );
 }
 
 export default ChoseUs;
--- src/Components/landing/LandingComponent/Landing.jsx
+++ src/Components/landing/LandingComponent/Landing.jsx
@@ -1,8 +1,6 @@
 import "../main.css";
-import { useSelector } from "react-redux";
 import androidphone from "../../../assets/androidphone.jpg";
 function Landing() {
-  let darkmode = useSelector((state) => state.darkmode.darkmode);
   return (
     <article className="h-screen w-screen bg-[#211060] relative">
       <div className="h-full w-96 absolute z-0 right-[16%] bg-[#28B8B0]"></div>
@@ -12,15 +10,13 @@ function Landing() {
         alt=""
       />
       <div
-        className={`w-[700px] h-[350px] flex justify-between flex-col absolute px-6 py-6 left-[14%] z-30 bottom-36 ${
-          darkmode ? "ligth-mode" : "dark-mode"
-        } `}
+        className={`w-[700px] h-[350px] flex justify-between flex-col absolute px-6 py-6 left-[14%] z-30 bottom-36  bg-[#fffafb] `}
       >
-        <h1 className={`intro ${darkmode ? "text-black" : "text-white"}`}>
+        <h1 className={`intro `}>
           Welcome to <span className="underline text-[#28B8B0]">CoinFizz</span>{" "}
           : the future of digital currency.
         </h1>
-        <p className="text-2xl font-normal">
+        <p className="text-2xl text-black font-normal">
           {" "}
           the future of digital currency. Embrace blockchain technology for
           secure, efficient, and decentralized transactions.
